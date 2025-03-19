32124f27194a83c442234301661ef1dfb3dc7a56
KarenEhab53 <karenehab53@gmail.com>
2025-03-19 23:56:57 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/My-portfolio/", // Ensure this matches your repo name
+  base: "/My-portfolio/", // Make sure this matches your GitHub repo name exactly
 });
