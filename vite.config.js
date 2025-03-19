78ab17e83f0b8bca796bfdeac7598bb5065520c2
KarenEhab53 <karenehab53@gmail.com>
2025-03-19 23:47:44 +0200

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'tailwindcss': 'tailwindcss/lib/index.js', // Force CommonJS resolution
-    },
-  },
+  base: "/My-portfolio/", // Ensure this matches your repo name
 });
